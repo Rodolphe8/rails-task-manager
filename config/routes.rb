986b5ff71f0d2166b7a6f08f9821dcb95647cf8a
Rodolphe Desobry <rodolphe.desobry@grenoble-em.com>
2019-02-19 17:34:21 +0100

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new", as: :create 
+  get "/tasks/edit/:id", to: "tasks#edit", as: :edit 
+  get "/tasks/:id", to: "tasks#show", as: :task 
+  post "/tasks", to: "tasks#create"
 end
